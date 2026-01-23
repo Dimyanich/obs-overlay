fdd2e8ca61c4e9c61c9282d6105e56b8eb18a3a5
Dimyanich <125753889+Dimyanich@users.noreply.github.com>
2026-01-23 19:57:16 +0300

    Update players.js

--- players.js
+++ players.js
@@ -3,15 +3,15 @@
 // Данные игроков
 const playersData = [
   { name: "Балтика", img: "photos/1.png" },
-  { name: "Фурия", img: "photos/2.png" },
+  { name: "Паника", img: "photos/2.png" },
   { name: "Линда", img: "photos/3.png" },
   { name: "Зеленоглазая", img: "photos/4.png" },
-  { name: "Саня", img: "photos/5.png" },
-  { name: "Метафора", img: "photos/6.png" },
-  { name: "Аннабель", img: "photos/7.png" },
+  { name: "Черешня", img: "photos/5.png" },
+  { name: "Суетолог", img: "photos/6.png" },
+  { name: "Annreal", img: "photos/7.png" },
   { name: "Бэтмен", img: "photos/8.png" },
   { name: "Ксю", img: "photos/9.png" },
-  { name: "Пиксель", img: "photos/10.png" }
+  { name: "Сойер", img: "photos/10.png" }
 ];
 
 // Роли (ключи — английские!)
